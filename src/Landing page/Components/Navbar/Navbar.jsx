52b93e5f2cb0d197e51d9b0563407fe8a87e8967
inaradfence <cyberdfence3@gmail.com>
2024-09-02 10:16:44 +0500

    landing page

--- src/Landing page/Components/Navbar/Navbar.jsx
+++ src/Landing page/Components/Navbar/Navbar.jsx
@@ -4,29 +4,40 @@ import { Link } from 'react-scroll';
 const Navbar = () => {
   return (
     <div className='container'> 
-    <nav className='navbar navbar-light bg-light navbar-expand-lg fixed-top'>
-        <span className='navbar-brand mb-0 h1'>
-            Navbar
-        </span>
-
-        {/* <ul className='navbar fs-3'>
-            <Link to='/'>
-            <li>Welcome</li>
-            </Link> 
-            
-            <Link to='showcase'>
-            <li>Showcase</li>
-            </Link> 
-            
-            <Link to='howitWorks'>
-            <li>How it Works?</li>
-            </Link> 
-            
-            <Link to='pricing'>
-            <li>Pricing</li>
-            </Link> 
-            
-        </ul> */}
+    <nav className='navbar navbar-light bg-light navbar-expand-sm fixed-top'>
+    <a
+    href='#'
+    className='navbar-brand mb-0 h1'>
+        <img
+        className='d-inline-block align-top'
+     src="../../Logo/PM.png" alt="" 
+        width="30" height="30"/>
+        Navbar
+    </a>               
+    <div className="collapse navbar-collapse">
+        <ul className="navbar-nav">
+            <li className="navbar-itme active">
+                <a href="#" className='nav-link active'>
+                Welcome
+                </a>
+            </li>
+            <li className="navbar-itme active">
+                <a href="#" className='nav-link'>
+                Showcase
+                </a>
+            </li>
+            <li className="navbar-itme active">
+                <a href="#" className='nav-link'>
+                How it works?
+                </a>
+            </li>
+            <li className="navbar-itme active">
+                <a href="#" className='nav-link'>
+                Pricing
+                </a>
+            </li>   
+        </ul>
+    </div>
     </nav>
     </div>
   )

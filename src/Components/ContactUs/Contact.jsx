2e87258234a39f4a5079b5a9de8f2217a059865c
Munawar <munawarhussa.7@gmail.com>
2024-10-06 13:15:43 +0500

    integrate email.js for contac

--- src/Components/ContactUs/Contact.jsx
+++ src/Components/ContactUs/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import SectionWrapper from '../../elements/SectionWrapper'
 import Button from '../../elements/Button'
 import Scene from '../../elements/Scene'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log("serce id ", process.env.REACT_APP_SERVICE_ID);
+        console.log("serce tmepla  ", process.env.REACT_APP_TEMPLATE_ID);
+        console.log("publijc id ", process.env.REACT_APP_PUBLIC_KEY);
+        
+        emailjs
+            .sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID,
+              form.current, {
+                publicKey: process.env.REACT_APP_PUBLIC_KEY,
+            })
+            .then(
+                () => {
+
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error);
+                },
+            );
+    };
+
     return (
         <SectionWrapper id={'contact'} title={'Contact Us'}>
             <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>
@@ -12,24 +37,45 @@ function Contact() {
                     {/* <img src='/contact-us.png' alt='contact' width={300} height={500} /> */}
                 </div>
                 <div className='bg-gray-100 p-5 rounded-lg'>
-                    <div className='my-3'>
-                        <h1 className='font-semibold mb-1 text-[20px] text-gray-600'>Your Email:</h1>
-                        <input placeholder='sample@gmail.com' className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full' />
-                    </div>
-                    <div className='my-4'>
-                        <h1 className='font-semibold mb-1 text-[20px] text-gray-600'>Subject:</h1>
-                        <input placeholder='Demo Request' className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full' />
-                    </div>
-                    <div className='my-3'>
-                        <h1 className='font-semibold mb-1 text-[20px] text-gray-600'>Message:</h1>
-                        <textarea rows={3} placeholder='sample@gmail.com' className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full' />
-                    </div>
-                    <div className='mt-4'>
-                        <Button
-                            variant='default'
-                            text={'Send Message'}
-                        />
-                    </div>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className='my-3'>
+                            <h1 className='font-semibold mb-1 text-[20px] text-gray-600'>Your Email:</h1>
+                            <input
+                                required={true}
+                                type='email'
+                                name='from_email'
+                                placeholder='sample@gmail.com'
+                                className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full'
+                            />
+                        </div>
+                        <div className='my-4'>
+                            <h1  className='font-semibold mb-1 text-[20px] text-gray-600'>Subject:</h1>
+                            <input
+                                type='text'
+                                required={true}
+                                name='subject'
+                                placeholder='Demo Request'
+                                className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full'
+                            />
+                        </div>
+                        <div className='my-3'>
+                            <h1 className='font-semibold mb-1 text-[20px] text-gray-600'>Message:</h1>
+                            <textarea
+                                rows={3}
+                                name='message'
+                                required={true}
+                                placeholder='sample@gmail.com'
+                                className='p-1 outline-gray-400 text-sm text-gray-500 rounded-md w-full'
+                            />
+                        </div>
+                        <div className='mt-4'>
+                            <Button
+                                type='submit'
+                                variant='default'
+                                text={'Send Message'}
+                            />
+                        </div>
+                    </form>
                 </div>
             </div>
         </SectionWrapper>
--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Button({ variant = 'default', text, onClick = () => { }, className = '' }) {
+function Button({ variant = 'default', text, onClick = () => { }, className = '', type = 'button' }) {
     const buttonVariant = {
         fill: 'bg-black text-white hover:bg-gray-300 hover:text-black',
         default: 'border border-gray-300 text-gray-600 hover:bg-gray-300  hover:text-black'
     }
     return (
         <button
+            type={type}
             onClick={onClick}
             className={`py-2 px-6 cursor-pointer transition-colors duration-300 ease-in-out rounded-full ${buttonVariant[variant]} ${className}`}
         >

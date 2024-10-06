dc4dbc3c113919b06f6cb9941ed131ab5b37ea41
Munawar <munawarhussa.7@gmail.com>
2024-10-06 11:29:23 +0500

    load portfolio template in iframe

--- src/Components/Templates/Templates.jsx
+++ src/Components/Templates/Templates.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { Tilt } from 'react-tilt'
+import CustomModal from '../../elements/CustomModel'
+import Button from '../../elements/Button'
+import Loader from '../../elements/Loader'
 
 const templateData = [
     {
@@ -9,35 +12,20 @@ const templateData = [
         image: '3d-portfolio.png'
     }, {
         title: 'Main page Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
+        link: 'https://spotfolio-mainpage-portfolio.vercel.app/',
         price: '20$',
         image: 'main-page-portfolio.png'
     }, {
-        title: '3D Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
+        title: 'Simple Full Stack Dev Portfolio',
+        link: 'https://spotfolio-simple-template.vercel.app/',
         price: '20$',
-        image: '3d-portfolio.png'
+        image: 'simple-portfolio.png'
     }, {
-        title: '3D Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
+        title: 'Spotfolio Animated Portfolio',
+        link: 'https://spotfolio-animated-template.vercel.app/',
         price: '20$',
-        image: '3d-portfolio.png'
-    }, {
-        title: '3D Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
-        price: '20$',
-        image: '3d-portfolio.png'
-    }, {
-        title: '3D Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
-        price: '20$',
-        image: '3d-portfolio.png'
-    }, {
-        title: '3D Portfolio',
-        link: 'https://munawar-hussain-portfolio.vercel.app/',
-        price: '20$',
-        image: '3d-portfolio.png'
-    },
+        image: 'animated-portfolio.png'
+    }
 ]
 
 const defaultOptions = {
@@ -55,9 +43,43 @@ const defaultOptions = {
 
 function Templates() {
     const [open, setOpen] = useState(false)
+    const [selectedTemplate, setSelectedTemplate] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleClose = () => {
+        setOpen(false)
+        setIsLoading(false)
+        setSelectedTemplate(null)
+    }
+
+    const handleIframeLoad = () => {
+        setIsLoading(false)
+    }
 
     return (
         <section id='templates' className='px-1 bg-white md:px-10 py-16'>
+            <CustomModal
+                open={open}
+                title={selectedTemplate?.title}
+                setOpen={setOpen}
+            >
+                <div className='px-2'>
+                    {isLoading && (
+                        <div className="flex flex-col justify-center items-center h-[80vh] w-[80vw]">
+                            <Loader />
+                            Loading...
+                        </div>
+                    )}
+                    <iframe
+                        src={selectedTemplate?.link}
+                        className={`h-[80vh] w-[80vw] ${isLoading ? 'hidden' : ''}`}  // Hide iframe when loading
+                        onLoad={handleIframeLoad}  // Trigger when iframe is loaded
+                    />
+                    <div className='flex w-full pb-2 gap-x-3 items-center justify-between'>
+                        <Button text={'Close'} onClick={handleClose} className='px-9' />
+                    </div>
+                </div>
+            </CustomModal>
             <h2 className="text-4xl font-bold text-gray-800 mb-8 text-center">
                 Templates
             </h2>
@@ -68,6 +90,8 @@ function Templates() {
                             <div
                                 onClick={() => {
                                     setOpen(true)
+                                    setSelectedTemplate(item)
+                                    setIsLoading(true)
                                 }}
                                 className='m-2 md:m-3 w-[300px] h-[400px] lg:w-[400px] lg:h-[500px] cursor-pointer'
                             >
